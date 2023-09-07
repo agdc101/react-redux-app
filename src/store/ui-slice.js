83237dcc7370754ce1e1a41033a7ce33ec87d6e3
agdc101 <agdc101@gmail.com>
2023-09-07 18:49:10 +0100

    reducer fix. state added for cart

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,14 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartVisible: false },
+    initialState: { cartIsVisible: false },
     reducers: {
         toggle(state) {
-            state.cartVisible = !state.cartVisible;
+            state.cartIsVisible = !state.cartIsVisible;
         }
     }
 });
 
 export const uiActions = uiSlice.actions;
 
-export default uiSlice.reducer;
+export default uiSlice;
