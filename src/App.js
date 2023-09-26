5c9db5953b0e38d3efb9764c61ffd124ef2ac582
agdc101 <agdc101@gmail.com>
2023-09-26 20:29:46 +0100

    fetch actions added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, Fragment } from 'react';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -16,13 +16,18 @@ function App() {
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
+  useEffect(() => {
     if (isInitial) {  
       isInitial = false;
       return;
     }
 
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
 
   }, [cart, dispatch]);
 
